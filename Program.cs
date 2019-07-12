fb3477af1cfd19d5657daf63d9b69cdbcae43c63
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-07-12 13:46:16 -0500

    finished all exercises

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 
 namespace Linq_Exercises
 {
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Define a customer
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -41,7 +54,7 @@ namespace Linq_Exercises
             IEnumerable<string> descend = from name in names
                                           orderby name descending
                                           select name;
-            descend.ToList().ForEach(name => Console.Write($"{name} "));
+            descend.ToList().ForEach(name => Console.WriteLine($"{name} "));
             Console.WriteLine("");
             Console.WriteLine("-------------");
 
@@ -65,6 +78,100 @@ namespace Linq_Exercises
             int count = numberCount.Count();
             Console.WriteLine(count);
             Console.WriteLine("-------------");
+
+            List<double> purchases = new List<double>()
+                {
+                    2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+                };
+
+            double totalAmount = purchases.Sum();
+            Console.WriteLine(totalAmount);
+            Console.WriteLine("-------------");
+
+            List<double> prices = new List<double>()
+                {
+                    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+                };
+
+            double highestValue = prices.Max();
+            Console.WriteLine(highestValue);
+            Console.WriteLine("-------------");
+
+            // Partitioning Operations
+            List<int> wheresSquaredo = new List<int>()
+                {
+                    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+                };
+
+            IEnumerable<int> firstSquareRt = wheresSquaredo.TakeWhile(number =>
+            {
+                var num = Convert.ToInt32(Math.Sqrt(number));
+                return num * num != number;
+            });
+            firstSquareRt.ToList().ForEach(number => Console.WriteLine(number));
+            Console.WriteLine("-------------");
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+            };
+
+            IEnumerable<Customer> millionaires = customers.Where(person => person.Balance > 1000000);
+
+            var millGroup = millionaires.GroupJoin(banks,
+                mill => mill.Bank,
+                sym => sym.Symbol,
+                (mill, sym) => new Result(mill.Name, sym));
+
+            // var millionDollarGroup =
+            //     from mill in millionaires
+            //     join sym in banks on mill equals sym.Symbol into group
+            //         select new { Name = mill, bankName = group };
+
+            foreach (var result in millGroup)
+            {
+                Console.WriteLine(result.name + ":");
+                foreach (var person in result.sym)
+                {
+                    Console.WriteLine(" " + person.Name);
+                }
+            }
+
+
+            var millionairesByBalance = from millionaire in millionaires
+                                        group millionaires by millionaire.Bank into mills
+                                        orderby mills.Key
+                                        select mills;
+
+
+
+        }
+    }
+
+    public class Result
+    {
+        public string name;
+        public IEnumerable<Bank> sym;
+
+        public Result(string name, IEnumerable<Bank> sym)
+        {
+            this.name = name;
+            this.sym = sym;
         }
     }
 }
